9ff1168439167b98fb2a5e4fec79ceb39476b43f
adepegba1 <adepegbadavid@rocketmail.com>
2025-11-15 20:47:28 +0100

    1. Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-// import RecipeList from "./components/RecipeList";
-// import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import DeleteRecipeButton from "./components/DeleteRecipeButton";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
