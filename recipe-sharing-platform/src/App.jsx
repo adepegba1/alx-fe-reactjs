e19f4a0f26e102e09dbbfb304377605328644c03
adepegba1 <adepegbadavid@rocketmail.com>
2025-12-02 22:15:06 +0100

    1. Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,8 @@
+import Homepage from "./components/Homepage";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
+      <Homepage />
     </>
   );
 }
---
+++ recipe-sharing-platform/src/components/Homepage.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+function Homepage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("../src/data.json")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+  return (
+    <>
+      <div>
+        {data.map((items) => (
+          <div
+            key={items.id}
+            className="flex flex-col bg-red-200 border-2 max-w-md m-4 p-4"
+          >
+            <img src={items.image} alt={items.title} />
+            <h2>{items.title}</h2>
+            <p>{items.summary}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+export default Homepage;
