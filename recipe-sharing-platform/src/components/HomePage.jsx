f43a3ac18675229a2c8b5b7b779c81bf2629e2ed
adepegba1 <adepegbadavid@rocketmail.com>
2025-12-03 19:22:26 +0100

    2. Building the Recipe Detail Page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   const [data, setData] = useState([]);
@@ -9,6 +10,7 @@ function HomePage() {
       .then((data) => setData(data))
       .catch((error) => console.error("Error:", error));
   }, []);
+
   return (
     <>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
@@ -17,13 +19,22 @@ function HomePage() {
             key={items.id}
             className="bg-red-200 border-2 max-w-md m-4 p-4 rounded-lg hover:scale-105 transition-transform shadow-lg"
           >
-            <img
-              className="w-full h-48 object-cover rounded-md"
-              src={items.image}
-              alt={items.title}
-            />
-            <h2 className="text-white font-bold text-3xl">{items.title}</h2>
-            <p className="text-xl">{items.summary}</p>
+            <Link to={`/recipe/${items.id}`}>
+              {" "}
+              <img
+                className="w-full h-48 object-cover rounded-md"
+                src={items.image}
+                alt={items.title}
+              />
+            </Link>
+
+            <Link to={`/recipe/${items.id}`}>
+              <h2 className="text-white font-bold text-3xl">{items.title}</h2>
+            </Link>
+
+            <Link to={`/recipe/${items.id}`}>
+              <p className="text-xl">{items.summary}</p>
+            </Link>
           </div>
         ))}
       </div>
